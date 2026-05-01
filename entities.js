7d4b77546fc83e06151f5304478e0ff1cddae444
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-04-30 18:23:41 -0700

    Remove maxInventorySize from player entity
    Removed maxInventorySize property from player entity.

--- entities.js
+++ entities.js
@@ -24,8 +24,7 @@ class Player extends Entity {
         this.xp = 0;
         this.maxXp = 500;
         this.gold = 0;
-        this.inventory = [];
-        this.maxInventorySize = 20; // Max items player can hold 
+        this.inventory = []; 
         
         this.equipment = {
             head: null, cape: null, amulet: null,
