98e60cd6a278e91f647c470fca4bff202cff7491
VIC <vic@VICs-iMac.local>
2026-09-10 00:39:25 -0700

    non-credential-text-input4CHAT

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-earth-v2.8.003';
+const CACHE_NAME = 'elden-earth-v2.8.004';
 
 const ASSETS_TO_CACHE = [
     './',
