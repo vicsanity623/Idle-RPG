f491e4acc9ea6f36866f206029dc076aca96b1e2
VIC <vic@VICs-iMac.local>
2026-09-10 00:46:47 -0700

    CitadelRejectionFix+FeedSilencerForRelocations

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-earth-v2.8.004';
+const CACHE_NAME = 'elden-earth-v2.8.005';
 
 const ASSETS_TO_CACHE = [
     './',
