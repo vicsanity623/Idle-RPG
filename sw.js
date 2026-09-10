b2fed313ab45f3d98b63e3f14119e4d50cf21dd7
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-09-10 03:32:36 -0700

    Update cache version to va0.1.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy an update!
-const CACHE_NAME = 'elden-earth-v9.5.6';
+const CACHE_NAME = 'elden-earth-va0.1.1';
 
 const ASSETS_TO_CACHE = [
     './',
